ed0d1f45fe8df201ebddfb2323139b56bd5e46bf
Mikel Giffin <giffinmike@hotmail.com>
2024-07-05 11:46:21 -0700

    heroku deployment shaky but working still1

--- backend/server.ts
+++ backend/server.ts
@@ -25,12 +25,12 @@ app.use(express.json()); // Add this line to parse JSON bodies
 // API ROUTES
 app.get('/api/hello', (req: Request, res: Response) => {
   console.log('Received request for /api/hello');
-  res.json({ message: 'Hello from the pannnn!' });
+  res.json({ message: 'Hello from the tipp!' });
 });
 
 app.get('/api/message', (req: Request, res: Response) => {
   console.log('Received request for /api/message');
-  res.json({ message: 'This is a test message from the pannnn!' });
+  res.json({ message: 'This is a test message from the tipp!' });
 });
 
 // Serve the static files from the React app
