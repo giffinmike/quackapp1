13481fd0776ec5c134d433dd92ab0b905e40a3e9
Mikel Giffin <giffinmike@hotmail.com>
2024-07-04 18:21:10 -0700

    working2

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ const fetchMessage = async () => {
 
   return (
     <div>
-      <h1>Test API Requests adaasdfsdf  sssdsfdf</h1>
+      <h1>Test API Requests adaasdfsdf  </h1>
       <button onClick={fetchHello}>Fetch Hello</button>
       <p>{helloResponse}</p>
       <button onClick={fetchMessage}>Fetch Message</button>
