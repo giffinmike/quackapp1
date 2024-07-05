3307dace220fa9a3b06dac6b78ebd40cb0664495
Mikel Giffin <giffinmike@hotmail.com>
2024-07-05 12:09:14 -0700

    double checking deploy1

--- backend/server.ts
+++ backend/server.ts
@@ -25,12 +25,12 @@ app.use(express.json()); // Add this line to parse JSON bodies
 // API ROUTES
 app.get('/api/hello', (req: Request, res: Response) => {
   console.log('Received request for /api/hello');
-  res.json({ message: 'Hello from the eee!' });
+  res.json({ message: 'Hello from the qqqqq!' });
 });
 
 app.get('/api/message', (req: Request, res: Response) => {
   console.log('Received request for /api/message');
-  res.json({ message: 'This is a test message from the eee!' });
+  res.json({ message: 'This is a test message from the qqqqq!' });
 });
 
 // Serve the static files from the React app
